3556c19cd320a476c2bfc0eaea76ecee13c16884
ssofiyas <sos00065@laurea.fi>
2025-10-27 22:16:55 +0200

    remove unnecessary changes

--- osa_3/frontend/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => (
-  <div style={{ marginTop: '2em', color: '#888' }}>
-    <hr />
-    <p>Fullstack Open 2025 &copy; Sofiya Sakhchinskaya</p>
-  </div>
-);
-
-export default Footer;
