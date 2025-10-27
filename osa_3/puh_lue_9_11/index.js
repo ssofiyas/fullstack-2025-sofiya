c36dedbc789192b6d2b27d6fae02365750a3804c
ssofiyas <sos00065@laurea.fi>
2025-10-27 12:18:22 +0200

    tehtavien 9-11 toinen vestio + frontend

--- osa_3/puh_lue_9_11/index.js
+++ osa_3/puh_lue_9_11/index.js
@@ -1,103 +1,63 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-
-app.use(express.static('dist'))
-
-dotenv.config()
 
 const app = express()
 
-
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
-
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-const requestLogger = (request, _response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-app.use(requestLogger)
-
-
-const mongoUrl = process.env.MONGODB_URI
-if (mongoUrl) {
-  console.log('Connecting to MongoDB...')
-  mongoose.connect(mongoUrl)
-    .then(() => {
-      console.log('Connected to MongoDB ✅')
-    })
-    .catch((error) => {
-      console.error('Error connecting to MongoDB:', error.message)
-    })
-} else {
-  console.log('No MongoDB URI found. Running without database ⚙️')
-}
-
-
-let notes = [
-  { id: 1, content: 'HTML is easy', important: true },
-  { id: 2, content: 'Browser can execute only JavaScript', important: false },
-  { id: 3, content: 'GET and POST are the most important methods of HTTP protocol', important: true }
+let persons = [
+  { id: 1, name: 'Arto Hellas', number: '040-123456' },
+  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+  { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+  { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' },
 ]
 
-
-app.get('/', (_req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
-app.get('/api/notes', (_req, res) => {
-  res.json(notes)
+app.get('/info', (req, res) => {
+  const info = `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `
+  res.send(info)
 })
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
+  const person = persons.find(p => p.id === id)
+  person ? res.json(person) : res.status(404).end()
 })
 
-app.delete('/api/notes/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(p => p.id !== id)
   res.status(204).end()
 })
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if (!body.content) {
-    return res.status(400).json({ error: 'content missing' })
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number missing' })
   }
 
-  const note = {
+  const person = {
     id: Math.floor(Math.random() * 10000),
-    content: body.content,
-    important: body.important || false
+    name: body.name,
+    number: body.number
   }
 
-  notes = notes.concat(note)
-  res.json(note)
+  persons = persons.concat(person)
+  res.json(person)
 })
-
-
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
