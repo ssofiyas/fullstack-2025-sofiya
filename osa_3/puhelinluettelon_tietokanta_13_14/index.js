43804f5193928e06cc202ce1f1c251693edb2a8a
ssofiyas <sos00065@laurea.fi>
2025-11-04 16:26:50 +0200

    tehtavat 3.13 ja 14.03 :)

---
+++ osa_3/puhelinluettelon_tietokanta_13_14/index.js
@@ -0,0 +1,94 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import Person from './models/person.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(morgan('tiny'))
+
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+})
+
+// GET, id:n perusteella
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+// delete henkilon
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+// post uusi henkilo
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing',
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+// kasittele virheet
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
